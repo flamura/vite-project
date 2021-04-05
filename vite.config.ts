78e0154285fc4deb80492e58be93b6767aba2c63
flamura <15041872+flamura@users.noreply.github.com>
2021-04-05 22:19:23 +0800

    fix: gh-page base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import legacy from '@vitejs/plugin-legacy'
 import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.GITHUB_WORKFLOW ? 'vite-project/' : './',
   resolve: {
     alias: {
       // to remove
